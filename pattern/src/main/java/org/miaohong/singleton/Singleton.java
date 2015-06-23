b1b21f5e2fd7b45f45bba1bcc839235b2e48a782
oikomi <miaohonghit@gmail.com>
2015-06-23 13:13:27 +0800

    init

--- pattern/src/main/java/org/miaohong/singleton/Singleton.java
+++ pattern/src/main/java/org/miaohong/singleton/Singleton.java
@@ -3,6 +3,10 @@ package org.miaohong.singleton;
 /**
  * Created by haroldmiao on 2015/6/23.
  */
+
+//使用静态内部类
+//这种写法仍然使用JVM本身机制保证了线程安全问题；由于 SingletonHolder
+// 是私有的，除了 getInstance() 之外没有办法访问它，因此它是懒汉式的；同时读取实例的时候不会进行同步，没有性能缺陷；也不依赖 JDK 版本。
 public class Singleton {
     private static class SingletonHolder {
         private static final Singleton INSTANCE = new Singleton();
@@ -18,7 +22,7 @@ public class Singleton {
     }
 }
 
-
+//线程级别的单例模式：通过ThreadLocal<T>类型的静态成员变量 定义 仅在线程内部生效的全局对象。
 class SingletonThread {
     private static final ThreadLocal<SingletonThread> local =
             new ThreadLocal<SingletonThread>() {
